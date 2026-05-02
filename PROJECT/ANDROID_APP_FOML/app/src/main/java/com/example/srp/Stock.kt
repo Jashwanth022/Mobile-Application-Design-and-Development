65609f72758b3c46e31ff7cf54b8cbaa54badfc3
JASHWANTH SV <231701023@rajalakshmi.edu.in>
2026-05-02 19:45:49 +0530

    Delete PROJECT directory

--- PROJECT/ANDROID_APP_FOML/app/src/main/java/com/example/srp/Stock.kt
+++
@@ -1,9 +0,0 @@
-package com.example.srp
-
-data class Stock(
-    val symbol: String,
-    val name: String,
-    val changePercent: Double,
-    val accuracy: Int,
-    val isTopPick: Boolean = false
-)
--- PROJECT/ANDROID_APP_FOML/app/src/main/java/com/example/srp/TrendingStock.kt
+++
@@ -1,7 +0,0 @@
-package com.example.srp
-
-data class TrendingStock(
-    val name: String,
-    val change: String,
-    val isPositive: Boolean
-)
